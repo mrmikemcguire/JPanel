6f0c46b35d36ac8385df45e3fa38ac2f3254bbf4
mcguire <mcguire@MU-C3-TEACHER01.mullen.pvt.k12.co.us>
2014-05-21 11:39:30 -0600

    A much cleaner pair of demos.

---
+++ JPanel/src/JOptionPaneDemo.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class JOptionPaneDemo extends JFrame
+	{
+	private static final long serialVersionUID = 1L;
+	static String name; 
+	static int className;
+	static JFrame frame = new JFrame();
+
+	public static void main(String[] args)
+	    {
+		name = JOptionPane.showInputDialog("What is your name?");  
+		JOptionPane.showMessageDialog(frame, "Hi, " + name);
+		Object[] options = {"Druid", "Ranger", "Monk"};
+		className = JOptionPane.showOptionDialog(frame, "What class would you like to be?",
+				"Your Vocation",
+				JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null, options, options[2]);
+		
+		switch(className)
+			{
+			case 0:
+				{
+				JOptionPane.showMessageDialog(frame, "Ah, a nature lover...God's first Bible...");
+				break;
+				}
+			case 1:
+				{
+				JOptionPane.showMessageDialog(frame, "Ah, another Aragorn wannabe, eh?");
+				break;
+				}
+			case 2:
+				{
+				JOptionPane.showMessageDialog(frame, "If I had to do it all over again... Trappist monk...");
+				break;
+				}
+			}
+	    }
+	}
